d892724423785a0ac5b49d3e2cd2c44aa6748d2f
vinotha2007-V <logiravi2002@gmail.com>
2026-04-30 19:59:41 +0530

    Create data_preprocessing.py

---
+++ 6. Data Preprocessing(src/data_preprocessing.py
@@ -0,0 +1,17 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+
+def load_data(path):
+    return pd.read_csv(path)
+
+def preprocess_data(df):
+    df = pd.get_dummies(df, drop_first=True)
+    
+    X = df.drop("price", axis=1)
+    y = df["price"]
+    
+    scaler = StandardScaler()
+    X_scaled = scaler.fit_transform(X)
+    
+    return train_test_split(X_scaled, y, test_size=0.2, random_state=42)
